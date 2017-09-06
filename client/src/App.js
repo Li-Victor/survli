15aeb0a1fbae70e6e4b684efcd4d288828b47d98
Victor Li <victorli5674@yahoo.com>
2017-09-06 13:19:07 -0700

    client link to google oauth

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,11 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Hi There</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <a href="/auth/google">Sign In With Google</a>
         </p>
       </div>
     );
