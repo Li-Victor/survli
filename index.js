927de13b50ebea2e0c97e6015d62d9cdc2da8d10
Victor Li <victorli5674@yahoo.com>
2017-09-06 10:05:20 -0700

    google oauth

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./services/passport');
+require('./models/User');
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
-});
+const app = express();
+mongoose.connect(keys.mongoURI);
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
