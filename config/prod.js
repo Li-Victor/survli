b4a040d51b4482278a70352f34911a952c9af67d
Victor Li <victorli5674@yahoo.com>
2017-09-06 09:13:49 -0700

    went back to the beginning

--- config/prod.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGOURI,
-  cookieKey: process.env.COOKIE_KEY
-}:
