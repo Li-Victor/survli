ba335e92aefe813b552132ac1b2e62f8d4679c74
Victor Li <victorli5674@yahoo.com>
2017-09-06 11:09:24 -0700

    separate keys for dev and prod

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// keys.js - figure out what set of credentials to return
+if(process.env.NODE_ENV === 'production') {
+  // we are in production - retun the prod set of keys
+  module.exports = require('./pro')
+} else {
+  // we are in devlopment - return the dev keys
+  module.exports = require('./dev');
+}
