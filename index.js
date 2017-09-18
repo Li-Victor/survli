d4b1ee5db89a5a28974793bb72d521b7aa7c465b
Victor Li <victorli5674@yahoo.com>
2017-09-17 23:40:36 -0700

    fixed linting problems on index.js

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
-const passport = require('passport')
+const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
+const path = require('path');
 require('./models/User');
 require('./models/Survey');
 require('./services/passport');
 
-mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
 app.use(bodyParser.json());
-
-app.use(cookieSession({
-  maxAge: 30 * 24 * 60 * 60 * 1000,
-  keys: [keys.cookieKey]
-}));
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -26,10 +27,9 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 require('./routes/surveyRoutes')(app);
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
-  const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
