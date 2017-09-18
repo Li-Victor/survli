d434073bf074159f5c8c8a1727988cfb6c3809d3
Victor Li <victorli5674@yahoo.com>
2017-09-18 00:02:07 -0700

    linting utils, actions, and reducers

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,21 +1,18 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => async dispatch => {
+export const fetchUser = () => async (dispatch) => {
   const res = await axios.get('/api/current_user');
-
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const handleToken = token => async dispatch => {
+export const handleToken = token => async (dispatch) => {
   const res = await axios.post('/api/stripe', token);
-
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const submitSurvet = (values, history) => async dispatch => {
+export const submitSurvey = (values, history) => async (dispatch) => {
   const res = await axios.post('/api/surveys', values);
-
   history.push('/surveys');
   dispatch({ type: FETCH_USER, payload: res.data });
 };
