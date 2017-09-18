71845da54caee1af29be76ec1a7ca5910df247b1
Victor Li <victorli5674@yahoo.com>
2017-09-18 09:43:07 -0700

    had to fix keys.js for heroku

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,8 @@
 // keys.js - figure out what set of credentials to return
-const prod = require('./prod');
-const dev = require('./dev');
-
 if (process.env.NODE_ENV === 'production') {
   // we are in production - retun the prod set of keys
-  module.exports = prod;
+  module.exports = require('./prod');
 } else {
   // we are in devlopment - return the dev keys
-  module.exports = dev;
+  module.exports = require('./dev');
 }
