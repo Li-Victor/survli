8e95f799a36d23b175f33a8679f213f97bf1fd1a
Victor Li <victorli5674@yahoo.com>
2017-09-17 23:25:12 -0700

    fixed linting problems with config folder

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,11 @@
 // keys.js - figure out what set of credentials to return
-if(process.env.NODE_ENV === 'production') {
+const prod = require('./prod');
+const dev = require('./dev');
+
+if (process.env.NODE_ENV === 'production') {
   // we are in production - retun the prod set of keys
-  module.exports = require('./prod')
+  module.exports = prod;
 } else {
   // we are in devlopment - return the dev keys
-  module.exports = require('./dev');
+  module.exports = dev;
 }
